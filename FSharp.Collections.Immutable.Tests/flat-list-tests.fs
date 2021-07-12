d232b99046373c3cf346e8400f8aae4f74b048bd
Andrey Sayapin <andrej.sajapin@gmail.com>
2021-07-12 12:57:53 +0300

    more tests

--- FSharp.Collections.Immutable.Tests/flat-list-tests.fs
+++ FSharp.Collections.Immutable.Tests/flat-list-tests.fs
@@ -8,15 +8,21 @@ module FlatListTestsUtils =
     let nullList<'a> () = new FlatList<'a>()
     let emptyList<'a> () = FlatList<'a>.Empty
     let list0to9 () = FlatList.init 10 id
+    let list9to0 () = FlatList.init 10 ((-) 9)
+    let listOf count list =
+        let builder = FlatList.builderWith count
+        for i = 0 to count do
+            builder.Add list
+        FlatList.ofBuilder builder
     let throwsOnlyOnNullLists category func = [
-        (func |> appliedTo (nullList<int> ()) |> throws) |> testCase "throws on null list" category
-        (func |> appliedTo (emptyList<int> ()) |> doesNotThrow) |> testCase "does not throw on empty list" category
-        (func |> appliedTo (list0to9 ()) |> doesNotThrow) |> testCase "does not throw on non-empty list" category
+        (func |> appliedToFunc nullList<int> |> throws) |> testCase "throws on null list" category
+        (func |> appliedToFunc emptyList<int> |> doesNotThrow) |> testCase "does not throw on empty list" category
+        (func |> appliedToFunc list0to9 |> doesNotThrow) |> testCase "does not throw on non-empty list" category
     ]
     let doesNotThrowOnlyOnFilledList category func = [
-        (func |> appliedTo (nullList<int> ()) |> throws) |> testCase "throws on null list" category
-        (func |> appliedTo (emptyList<int> ()) |> throws) |> testCase "throws on empty list" category
-        (func |> appliedTo (list0to9 ()) |> doesNotThrow) |> testCase "does not throw on non-empty list" category
+        (func |> appliedToFunc nullList<int> |> throws) |> testCase "throws on null list" category
+        (func |> appliedToFunc emptyList<int> |> throws) |> testCase "throws on empty list" category
+        (func |> appliedToFunc list0to9 |> doesNotThrow) |> testCase "does not throw on non-empty list" category
     ]
 
 type FlatListFixture () =
@@ -25,7 +31,7 @@ type FlatListFixture () =
         FlatList.toArray |> throwsOnlyOnNullLists (nameof(FlatList.toArray))
         FlatList.toList |> throwsOnlyOnNullLists (nameof(FlatList.toList))
         FlatList.length |> throwsOnlyOnNullLists (nameof(FlatList.length))
-        (flip FlatList.append <| emptyList<int> ()) |> throwsOnlyOnNullLists (nameof(FlatList.append) + " (first arg)")
+        (FlatList.append |> withSecondArgFrom emptyList<int>) |> throwsOnlyOnNullLists (nameof(FlatList.append) + " (first arg)")
         (FlatList.append <| emptyList<int> ()) |> throwsOnlyOnNullLists (nameof(FlatList.append) + " (second arg)")
         (FlatList.indexFromWith LanguagePrimitives.FastGenericEqualityComparer 0 1) |> throwsOnlyOnNullLists (nameof(FlatList.indexFromWith))
         (FlatList.indexFrom 0 1) |> throwsOnlyOnNullLists (nameof(FlatList.indexFrom))
@@ -42,19 +48,19 @@ type FlatListFixture () =
         FlatList.countBy noopPredicate |> throwsOnlyOnNullLists (nameof(FlatList.countBy))
         FlatList.indexed |> throwsOnlyOnNullLists (nameof(FlatList.indexed))
         FlatList.iter ignore |> throwsOnlyOnNullLists (nameof(FlatList.iter))
-        (flip (FlatList.iter2 ignore2) <| (emptyList ())) |> throwsOnlyOnNullLists (nameof(FlatList.iter2) + " (first arg)")
+        (FlatList.iter2 ignore2 |> withSecondArgFrom emptyList) |> throwsOnlyOnNullLists (nameof(FlatList.iter2) + " (first arg)")
         (FlatList.iter2 ignore2 (emptyList ())) |> throwsOnlyOnNullLists (nameof(FlatList.iter2) + " (second arg)")
         FlatList.distinct |> throwsOnlyOnNullLists (nameof(FlatList.distinct))
         FlatList.distinctBy noopPredicate |> throwsOnlyOnNullLists (nameof(FlatList.distinctBy))
-        (flip (FlatList.map2 ignore2) <| (emptyList ())) |> throwsOnlyOnNullLists (nameof(FlatList.map2) + " (first arg)")
+        (FlatList.map2 ignore2 |> withSecondArgFrom emptyList) |> throwsOnlyOnNullLists (nameof(FlatList.map2) + " (first arg)")
         (FlatList.map2 ignore2 (emptyList ())) |> throwsOnlyOnNullLists (nameof(FlatList.map2) + " (second arg)")
         (fun a -> FlatList.map3 ignore3 a (emptyList ()) (emptyList ())) |> throwsOnlyOnNullLists (nameof(FlatList.map3) + " (first arg)")
         (fun a -> FlatList.map3 ignore3 (emptyList ()) a (emptyList ())) |> throwsOnlyOnNullLists (nameof(FlatList.map3) + " (second arg)")
         (FlatList.map3 ignore3 (emptyList ()) (emptyList ())) |> throwsOnlyOnNullLists (nameof(FlatList.map3) + " (third arg)")
-        (flip (FlatList.mapi2 ignore3) <| (emptyList ())) |> throwsOnlyOnNullLists (nameof(FlatList.mapi2) + " (first arg)")
+        (FlatList.mapi2 ignore3 |> withSecondArgFrom emptyList) |> throwsOnlyOnNullLists (nameof(FlatList.mapi2) + " (first arg)")
         (FlatList.mapi2 ignore3 (emptyList ())) |> throwsOnlyOnNullLists (nameof(FlatList.mapi2) + " (second arg)")
         (FlatList.iteri ignore2) |> throwsOnlyOnNullLists (nameof(FlatList.iteri))
-        (flip (FlatList.iteri2 ignore3) <| (emptyList ())) |> throwsOnlyOnNullLists (nameof(FlatList.iteri2) + " (first arg)")
+        (FlatList.iteri2 ignore3 |> withSecondArgFrom emptyList) |> throwsOnlyOnNullLists (nameof(FlatList.iteri2) + " (first arg)")
         (FlatList.iteri2 ignore3 (emptyList ())) |> throwsOnlyOnNullLists (nameof(FlatList.iteri2) + " (second arg)")
         (FlatList.mapi ignore2) |> throwsOnlyOnNullLists (nameof(FlatList.mapi))
 
@@ -77,34 +83,60 @@ type FlatListFixture () =
     static member operationCases = [
         (FlatList.init 5 |> appliedTo (id) |> producesEquivalentOf [0..4]) |> testCase "for 5 elements yield valid list" (nameof(FlatList.init))
         (FlatList.init 0 |> appliedTo (id) |> producesEquivalentOf []) |> testCase "for 0 elements yields empty list" (nameof(FlatList.init))
-        (FlatList.isEmpty |> appliedTo (emptyList ()) |> produces true) |> testCase "empty list is" (nameof(FlatList.isEmpty))
-        (FlatList.isEmpty |> appliedTo (list0to9 ()) |> produces false) |> testCase "non-empty list is not" (nameof(FlatList.isEmpty))
-        (FlatList.isDefault |> appliedTo (nullList ()) |> produces true) |> testCase "null list is" (nameof(FlatList.isDefault))
-        (FlatList.isDefault |> appliedTo (emptyList ()) |> produces false) |> testCase "empty list is not" (nameof(FlatList.isDefault))
-        (FlatList.isDefault |> appliedTo (list0to9 ()) |> produces false) |> testCase "non-empty list is not" (nameof(FlatList.isDefault))
-        (FlatList.isDefaultOrEmpty |> appliedTo (nullList ()) |> produces true) |> testCase "null list is" (nameof(FlatList.isDefaultOrEmpty))
-        (FlatList.isDefaultOrEmpty |> appliedTo (emptyList ()) |> produces true) |> testCase "empty list is" (nameof(FlatList.isDefaultOrEmpty))
-        (FlatList.isDefaultOrEmpty |> appliedTo (list0to9 ()) |> produces false) |> testCase "non-empty list is not" (nameof(FlatList.isDefaultOrEmpty))
-        (FlatList.length |> appliedTo (emptyList ()) |> produces 0) |> testCase "for empty list is 0" (nameof(FlatList.length))
-        (FlatList.length |> appliedTo (list0to9 ()) |> produces 10) |> testCase "for non-empty list is .length" (nameof(FlatList.length))
-        (FlatList.item 0 |> appliedTo (emptyList ()) |> throws) |> testCase "throws for empty list" (nameof(FlatList.item))
-        (FlatList.item 0 |> appliedTo (list0to9 ()) |> produces 0) |> testCase "[0] for non-empty list equals to [0]" (nameof(FlatList.item))
-        (FlatList.item 5 |> appliedTo (list0to9 ()) |> produces 5) |> testCase "[5] for non-empty list equals to [5]" (nameof(FlatList.item))
-        (FlatList.item -1 |> appliedTo (list0to9 ()) |> throws) |> testCase "[-1] for non-empty list throws" (nameof(FlatList.item))
-        (FlatList.item 25 |> appliedTo (list0to9 ()) |> throws) |> testCase "[out of bounds] for non-empty list throws" (nameof(FlatList.item))
-        (FlatList.append (emptyList ()) |> appliedTo (list0to9 ()) |> producesEquivalentOf [0..9]) |> testCase "empty to non-empty" (nameof(FlatList.append))
-        (FlatList.append (list0to9 ()) |> appliedTo (emptyList ()) |> producesEquivalentOf [0..9]) |> testCase "non-empty to empty" (nameof(FlatList.append))
-        (FlatList.append (list0to9 ()) |> appliedTo (list0to9 ()) |> producesEquivalentOf (List.append [0..9] [0..9])) |> testCase "non-empty to non-empty" (nameof(FlatList.append))
-        (FlatList.indexRangeWith HashIdentity.Structural 3 5 6 |> appliedTo (list0to9 ()) |> produces 6) |> testCase "returns index for valid args" (nameof(FlatList.indexRangeWith))
-        (FlatList.indexRangeWith HashIdentity.Structural 3 15 6 |> appliedTo (list0to9 ()) |> throws) |> testCase "throws for invalid args (count)" (nameof(FlatList.indexRangeWith))
-        (FlatList.indexRangeWith HashIdentity.Structural -2 5 6 |> appliedTo (list0to9 ()) |> throws) |> testCase "throws for invalid args (index)" (nameof(FlatList.indexRangeWith))
-        (FlatList.indexRangeWith HashIdentity.Structural 3 5 0 |> appliedTo (list0to9 ()) |> produces -1) |> testCase "returns -1 for non-present item" (nameof(FlatList.indexRangeWith))
-        (FlatList.removeAll ([] |> FlatList.ofList) |> appliedTo (list0to9 ()) |> producesEquivalentOf [0..9]) |> testCase "with empty list - returns source" (nameof(FlatList.removeAll))
-        (FlatList.removeAll ([] |> FlatList.ofList) |> appliedTo (emptyList ()) |> producesEquivalentOf []) |> testCase "with empty list - returns source (even if empty)" (nameof(FlatList.removeAll))
-        (FlatList.removeAll (list0to9 ()) |> appliedTo (emptyList ()) |> producesEquivalentOf []) |> testCase "takes no action for not present values" (nameof(FlatList.removeAll))
-        (FlatList.removeAll (list0to9 ()) |> appliedTo (list0to9 ()) |> producesEquivalentOf []) |> testCase "removes all present items" (nameof(FlatList.removeAll))
-        (FlatList.filter (fun i -> i < 4) |> appliedTo (list0to9 ()) |> producesEquivalentOf [0..3]) |> testCase "yields only valid items" (nameof(FlatList.filter))
-        (FlatList.filter (fun i -> i > 100) |> appliedTo (list0to9 ()) |> producesEquivalentOf []) |> testCase "returns empty list if predicate matches no items" (nameof(FlatList.filter))
+        (FlatList.isEmpty |> appliedToFunc emptyList |> produces true) |> testCase "empty list is" (nameof(FlatList.isEmpty))
+        (FlatList.isEmpty |> appliedToFunc list0to9 |> produces false) |> testCase "non-empty list is not" (nameof(FlatList.isEmpty))
+        (FlatList.isDefault |> appliedToFunc nullList |> produces true) |> testCase "null list is" (nameof(FlatList.isDefault))
+        (FlatList.isDefault |> appliedToFunc emptyList |> produces false) |> testCase "empty list is not" (nameof(FlatList.isDefault))
+        (FlatList.isDefault |> appliedToFunc list0to9 |> produces false) |> testCase "non-empty list is not" (nameof(FlatList.isDefault))
+        (FlatList.isDefaultOrEmpty |> appliedToFunc nullList |> produces true) |> testCase "null list is" (nameof(FlatList.isDefaultOrEmpty))
+        (FlatList.isDefaultOrEmpty |> appliedToFunc emptyList |> produces true) |> testCase "empty list is" (nameof(FlatList.isDefaultOrEmpty))
+        (FlatList.isDefaultOrEmpty |> appliedToFunc list0to9 |> produces false) |> testCase "non-empty list is not" (nameof(FlatList.isDefaultOrEmpty))
+        (FlatList.length |> appliedToFunc emptyList |> produces 0) |> testCase "for empty list is 0" (nameof(FlatList.length))
+        (FlatList.length |> appliedToFunc list0to9 |> produces 10) |> testCase "for non-empty list is .length" (nameof(FlatList.length))
+        (FlatList.item 0 |> appliedToFunc emptyList |> throws) |> testCase "throws for empty list" (nameof(FlatList.item))
+        (FlatList.item 0 |> appliedToFunc list0to9 |> produces 0) |> testCase "[0] for non-empty list equals to [0]" (nameof(FlatList.item))
+        (FlatList.item 5 |> appliedToFunc list0to9 |> produces 5) |> testCase "[5] for non-empty list equals to [5]" (nameof(FlatList.item))
+        (FlatList.item -1 |> appliedToFunc list0to9 |> throws) |> testCase "[-1] for non-empty list throws" (nameof(FlatList.item))
+        (FlatList.item 25 |> appliedToFunc list0to9 |> throws) |> testCase "[out of bounds] for non-empty list throws" (nameof(FlatList.item))
+        (FlatList.append (emptyList ()) |> appliedToFunc list0to9 |> producesEquivalentOf [0..9]) |> testCase "empty to non-empty" (nameof(FlatList.append))
+        (FlatList.append (list0to9 ()) |> appliedToFunc emptyList |> producesEquivalentOf [0..9]) |> testCase "non-empty to empty" (nameof(FlatList.append))
+        (FlatList.append (list0to9 ()) |> appliedToFunc list0to9 |> producesEquivalentOf (List.append [0..9] [0..9])) |> testCase "non-empty to non-empty" (nameof(FlatList.append))
+        (FlatList.indexRangeWith HashIdentity.Structural 3 5 6 |> appliedToFunc list0to9 |> produces 6) |> testCase "returns index for valid args" (nameof(FlatList.indexRangeWith))
+        (FlatList.indexRangeWith HashIdentity.Structural 3 15 6 |> appliedToFunc list0to9 |> throws) |> testCase "throws for invalid args (count)" (nameof(FlatList.indexRangeWith))
+        (FlatList.indexRangeWith HashIdentity.Structural -2 5 6 |> appliedToFunc list0to9 |> throws) |> testCase "throws for invalid args (index)" (nameof(FlatList.indexRangeWith))
+        (FlatList.indexRangeWith HashIdentity.Structural 3 5 0 |> appliedToFunc list0to9 |> produces -1) |> testCase "returns -1 for non-present item" (nameof(FlatList.indexRangeWith))
+        (FlatList.removeAll ([] |> FlatList.ofList) |> appliedToFunc list0to9 |> producesEquivalentOf [0..9]) |> testCase "with empty list - returns source" (nameof(FlatList.removeAll))
+        (FlatList.removeAll ([] |> FlatList.ofList) |> appliedToFunc emptyList |> producesEquivalentOf []) |> testCase "with empty list - returns source (even if empty)" (nameof(FlatList.removeAll))
+        (FlatList.removeAll (list0to9 ()) |> appliedToFunc emptyList |> producesEquivalentOf []) |> testCase "takes no action for not present values" (nameof(FlatList.removeAll))
+        (FlatList.removeAll (list0to9 ()) |> appliedToFunc list0to9 |> producesEquivalentOf []) |> testCase "removes all present items" (nameof(FlatList.removeAll))
+        (FlatList.filter ((>) 4) |> appliedToFunc list0to9 |> producesEquivalentOf [0..3]) |> testCase "yields only valid items" (nameof(FlatList.filter))
+        (FlatList.filter ((<) 100) |> appliedToFunc list0to9 |> producesEquivalentOf []) |> testCase "returns empty list if predicate matches no items" (nameof(FlatList.filter))
+        (FlatList.removeRange 0 0 |> appliedToFunc emptyList |> producesEquivalentOf []) |> testCase "empty list - 0 items from 0" (nameof(FlatList.removeRange))
+        (FlatList.removeRange 0 -1 |> appliedToFunc emptyList |> throws) |> testCase "empty list - -1 items from 0" (nameof(FlatList.removeRange))
+        (FlatList.removeRange -1 0 |> appliedToFunc emptyList |> throws) |> testCase "empty list - 0 items from -1" (nameof(FlatList.removeRange))
+        (FlatList.removeRange 0 3 |> appliedToFunc emptyList |> throws) |> testCase "empty list - 3 items from 0" (nameof(FlatList.removeRange))
+        (FlatList.removeRange 0 3 |> appliedToFunc list0to9 |> producesEquivalentOf [3..9]) |> testCase "non-empty list - 3 items from 0" (nameof(FlatList.removeRange))
+        (FlatList.sortRangeWith LanguagePrimitives.GenericComparison 0 3 |> appliedToFunc emptyList |> throws) |> testCase "empty list - 3 items from 0" (nameof(FlatList.sortRangeWith))
+        (FlatList.sortRangeWith LanguagePrimitives.GenericComparison 0 -1 |> appliedToFunc emptyList |> throws) |> testCase "empty list - -1 items from 0" (nameof(FlatList.sortRangeWith))
+        (FlatList.sortRangeWith LanguagePrimitives.GenericComparison -1 0 |> appliedToFunc emptyList |> throws) |> testCase "empty list - 0 items from -1" (nameof(FlatList.sortRangeWith))
+        (FlatList.sortRangeWith LanguagePrimitives.GenericComparison 0 0 |> appliedToFunc emptyList |> producesEquivalentOf []) |> testCase "empty list - 3 items from 0" (nameof(FlatList.sortRangeWith))
+        (FlatList.sortRangeWith LanguagePrimitives.GenericComparison 0 3 |> appliedToFunc list9to0 |> producesSameAs [7;8;9;6;5;4;3;2;1;0]) |> testCase "9 to 0 list - 3 items from 0" (nameof(FlatList.sortRangeWith))
+        (FlatList.sortRangeWith (fun x y -> y - x) 0 3 |> appliedToFunc list0to9 |> producesSameAs [2;1;0;3;4;5;6;7;8;9]) |> testCase "0 to 9 list - 3 items from 0" (nameof(FlatList.sortRangeWith))
+        (FlatList.initWithValue -1 |> appliedTo 1 |> throws) |> testCase "-1 items" (nameof(FlatList.initWithValue))
+        (FlatList.initWithValue 0 |> appliedTo 1 |> producesEquivalentOf []) |> testCase "0 items" (nameof(FlatList.initWithValue))
+        (FlatList.initWithValue 3 |> appliedTo 1 |> producesEquivalentOf [1;1;1]) |> testCase "3 items" (nameof(FlatList.initWithValue))
+        (FlatList.concat |> appliedToFunc emptyList |> producesEquivalentOf []) |> testCase "no items" (nameof(FlatList.concat))
+        (FlatList.concat |> appliedTo (listOf 4 <| emptyList ()) |> producesEquivalentOf []) |> testCase "no items 4 times" (nameof(FlatList.concat))
+        (FlatList.concat |> appliedTo ([emptyList ();list0to9 ()] |> FlatList.ofList) |> producesEquivalentOf [0..9]) |> testCase "no items & 0 to 9" (nameof(FlatList.concat))
+        (FlatList.concat |> appliedTo ([list0to9 ();list0to9 ()] |> FlatList.ofList) |> producesEquivalentOf [0;1;2;3;4;5;6;7;8;9;0;1;2;3;4;5;6;7;8;9]) |> testCase "0 to 9 times 2" (nameof(FlatList.concat))
+        (FlatList.map ((+) 3) |> appliedToFunc emptyList |> producesEquivalentOf []) |> testCase "empty list" (nameof(FlatList.map))
+        (FlatList.map ((+) 3) |> appliedToFunc list0to9 |> producesEquivalentOf [3..12]) |> testCase "non-empty list" (nameof(FlatList.map))
+        (FlatList.countBy (fun x -> x % 2 = 0) |> appliedToFunc list0to9 |> producesEquivalentOf [true,5;false,5]) |> testCase "non-empty list" (nameof(FlatList.countBy))
+        (FlatList.countBy (fun x -> x % 2 = 0) |> appliedToFunc emptyList |> producesEquivalentOf []) |> testCase "empty list" (nameof(FlatList.countBy))
+        (FlatList.distinct |> appliedToFunc emptyList |> producesEquivalentOf []) |> testCase "empty list" (nameof(FlatList.distinct))
+        (FlatList.distinct |> appliedTo ([1;1;1;1] |> FlatList.ofList) |> producesEquivalentOf [1]) |> testCase "duplicates list" (nameof(FlatList.distinct))
+        (FlatList.distinctBy (fun x -> x % 2 = 0) |> appliedToFunc emptyList |> producesEquivalentOf []) |> testCase "empty list" (nameof(FlatList.distinctBy))
+        (FlatList.distinctBy (fun x -> x % 2 = 0) |> appliedToFunc list0to9 |> producesEquivalentOf [0;1]) |> testCase "0 to 9 list, oddity check" (nameof(FlatList.distinctBy))
     ]
 
     [<TestCaseSource(nameof(FlatListFixture.validationCases))>]
--- FSharp.Collections.Immutable.Tests/test-helpers.fs
+++ FSharp.Collections.Immutable.Tests/test-helpers.fs
@@ -6,12 +6,15 @@ open NUnit.Framework
 [<AutoOpen>]
 module TestHelpers =
     let appliedTo value func = fun () -> func value
+    let appliedToFunc value func = fun () -> func (value ())
     let produces value func = fun () -> () |> func |> should equal value
     let producesEquivalentOf value func = fun () -> () |> func |> should equivalent value
+    let producesSameAs value func = fun () -> () |> func |> Seq.fold2 (fun acc x y -> acc && (x = y)) true value |> should equal true
     let throws func = fun () -> Assert.Catch (func >> ignore) |> ignore
     let doesNotThrow func = fun () -> func () |> ignore |> should equal ()
     let noopPredicate _ = true
     let ignore2 _ _ = ()
     let ignore3 _ _ _ = ()
     let testCase name category (code:unit->unit) = ((TestCaseData code).SetName (category + " - " + name)).SetCategory category
-
+    let withSecondArg a f = fun b -> f b a
+    let withSecondArgFrom a f = fun b -> f b (a ())
